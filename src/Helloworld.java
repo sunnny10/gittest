57609a0a50193e46e81bf0778644127d5d379e12
Sun Jung <wjdtjs905@likelion.org>
2022-03-07 13:27:23 +0900

    b1 commit

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -2,7 +2,7 @@
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
-		System.out.println("Master");
+		System.out.println("b1");
 		System.out.println("test");
 	}
 }
